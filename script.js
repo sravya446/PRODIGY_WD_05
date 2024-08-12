0bbc12b2efb4ac47c4d2c550a57e303338d78e52
sravya446 <109335668+sravya446@users.noreply.github.com>
2024-08-12 13:57:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
+
+$(document).ready(function () {
+    weatherFn('Pune');
+});
+
+async function weatherFn(cName) {
+    const tempUrl = `${url}?q=${cName}&appid=${apiKey}&units=metric`;
+    try {
+        const res = await fetch(tempUrl);
+        const data = await res.json();
+        if (res.ok) {
+            weatherShowFn(data);
+        } else {
+            alert('City not found. Please try again.');
+        }
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        alert('Failed to fetch weather data. Please check your network connection and try again.');
+    }
+}
+
+function weatherShowFn(data) {
+    $('#city-name').text(data.name);
+    $('#date').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    $('#temperature').html(`${data.main.temp}°C`);
+    $('#description').text(data.weather[0].description);
+    $('#wind-speed').html(`Wind Speed: ${data.wind.speed} m/s`);
+    $('#weather-icon').attr('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    $('#weather-info').fadeIn();
+}
